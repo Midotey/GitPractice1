29d21fe4ca02aafe6b2435c6a64d651c7bd48d88
Midotey <shin.alexey2@gmail.com>
2024-04-25 14:04:24 +0300

    bug fix 5

--- GitPractice1/Form1.Designer.cs
+++ GitPractice1/Form1.Designer.cs
@@ -66,6 +66,7 @@
             button1.Name = "button1";
             button1.Size = new Size(110, 59);
             button1.TabIndex = 3;
+            button1.Text = "Convert";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
